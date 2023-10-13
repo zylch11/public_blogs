a98ab7bfa7c8119cd4d98cc77d38bab364b60f18
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2023-10-13 19:50:13 +0500

    Deleted old person model and repository

--- api-rate-limiting-using-drools/api-rate-limiting-using-drools/src/main/java/io/waqasahmed/apiratelimitingusingdrools/PersonRepositoryOld.java
+++
@@ -1,7 +0,0 @@
-package io.waqasahmed.apiratelimitingusingdrools;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepositoryOld extends JpaRepository<PersonOld, Long> {
-  PersonOld findByName(String name);
-}
