8feb3f5dc946fdb6798aa26ade6ebbb0bf40b451
Waqas Ahmed Rehmani <waqasahmed.rehmani@uworx.co.uk>
2023-10-14 15:13:47 +0500

    Removed redundant dependencies

--- api-rate-limiting-using-drools/api-rate-limiting-using-drools/src/main/java/io/waqasahmed/apiratelimitingusingdrools/ApiRateLimitingUsingDroolsApplication.java
+++ api-rate-limiting-using-drools/api-rate-limiting-using-drools/src/main/java/io/waqasahmed/apiratelimitingusingdrools/ApiRateLimitingUsingDroolsApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableAutoConfiguration
--- api-rate-limiting-using-drools/api-rate-limiting-using-drools/src/main/java/io/waqasahmed/apiratelimitingusingdrools/config/DroolsConfig.java
+++ api-rate-limiting-using-drools/api-rate-limiting-using-drools/src/main/java/io/waqasahmed/apiratelimitingusingdrools/config/DroolsConfig.java
@@ -1,46 +1,25 @@
 package io.waqasahmed.apiratelimitingusingdrools.config;
 
-import org.appformer.maven.integration.MavenRepository;
-import org.drools.compiler.kie.builder.impl.InternalKieModule;
 import org.kie.api.KieBase;
 import org.kie.api.KieServices;
-import org.kie.api.builder.KieBuilder;
-import org.kie.api.builder.KieFileSystem;
-import org.kie.api.builder.KieModule;
-import org.kie.api.builder.KieRepository;
 import org.kie.api.builder.KieScanner;
 import org.kie.api.builder.ReleaseId;
-import org.kie.api.builder.model.KieModuleModel;
-import org.kie.api.io.Resource;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
-import org.kie.internal.io.ResourceFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.io.File;
-import java.io.IOException;
-
 @Configuration
 public class DroolsConfig {
-
-  private static final String drlFile = "api-rules.drl";
-
   @Bean
   public KieContainer kieContainer() {
     KieServices kieServices = KieServices.Factory.get();
-//    ReleaseId releaseId = kieServices.newReleaseId("io.waqasahmed", "api-kjar", "1.0-SNAPSHOT");
-//    KieContainer kieContainer = kieServices.newKieContainer(releaseId);
-    KieContainer kieContainer = kieServices.getKieClasspathContainer();
-//    KieScanner kieScanner = kieServices.newKieScanner(kieContainer);
-//    kieScanner.start(5000L);
-    return kieContainer;
-  }
+    ReleaseId releaseId = kieServices.newReleaseId("io.waqasahmed", "api-kjar", "1.0-SNAPSHOT");
+    KieContainer kieContainer = kieServices.newKieContainer(releaseId);
 
-  @Bean
-  public KieSession kieSession() {
-    KieBase kieBase = kieContainer().getKieBase();
-    return kieBase.newKieSession();
+    KieScanner kScanner = kieServices.newKieScanner( kieContainer );
+    kScanner.start( 10000L );
+
+    return kieContainer;
   }
 }
